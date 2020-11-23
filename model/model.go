db8f4fce8e075af30e730f51f6db6093e115eb04
Jon Anderson <jon.david.anderson@gmail.com>
2020-11-23 12:49:31 -0800

    Fixed file naming issue.

--- model/model.go
+++ model/model.go
@@ -1,1 +1,17 @@
 package model
+
+import "github.com/apple/foundationdb/bindings/go/src/fdb/tuple"
+
+type (
+	Query struct {
+		Key   *Key
+		Value *Value
+	}
+
+	Key struct {
+		Directory []string
+		Tuple     tuple.Tuple
+	}
+
+	Value string
+)
