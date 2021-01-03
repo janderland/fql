1d5b2417b22aecddfcd0f01af0e9fffa7f7e68df
Jon Anderson <jon.david.anderson@gmail.com>
2021-01-03 15:36:04 -0800

    Changed allowed types in KeyValue.

--- keyval/keyval.go
+++ keyval/keyval.go
@@ -25,14 +25,13 @@ type (
 	Directory []interface{}
 
 	// A Tuple is equivalent to an FDB tuple. This type may
-	// contain nil, bool, string, int64, uint64, float64,
-	// UUID, or Variable.
+	// contain nil, bool, string, int64, float64, UUID, or
+	// Variable.
 	Tuple []interface{}
 
 	// A Value represents an FDB value stored alongside
 	// a key. This type may contain nil, bool, string,
-	// int64, uint64, float64, UUID, Tuple, Variable,
-	// or Clear.
+	// uint64, float64, UUID, Tuple, Variable, or Clear.
 	Value interface{}
 
 	// A UUID is equivalent to an FDB UUID.
