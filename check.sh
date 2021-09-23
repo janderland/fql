54a0bf8e0f2e56d95900b86846411bda3b7cb29d
Jon Anderson <jon.david.anderson@gmail.com>
2021-09-22 21:14:22 -0700

    Added Dockerfile linting.

--- check.sh
+++ check.sh
@@ -3,6 +3,7 @@ set -exuo pipefail
 
 cd "${0%/*}"
 find . -type f -iname '*.sh' -print0 | xargs -0 shellcheck
+find . -type f -iname 'Dockerfile' -print0 | xargs -0 -n 1 hadolint
 golangci-lint run
 go build ./...
 go test ./...
