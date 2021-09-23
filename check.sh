acbf22195e8a0243941248d91681a5ce38920aae
Jon Anderson <jon.david.anderson@gmail.com>
2021-09-22 23:03:49 -0700

    Container Improvements (#40)
    * Increase version of deps in build container.
    
    * Include shellcheck in checks.
    
    * Upgrade FDB in GH workflow.
    
    * Added Dockerfile linting.
    
    * Run go build before golang ci lint.
    
    * Failed docker build if yum fails.

--- check.sh
+++ check.sh
@@ -2,6 +2,8 @@
 set -exuo pipefail
 
 cd "${0%/*}"
+find . -type f -iname '*.sh' -print0 | xargs -0 shellcheck
+find . -type f -iname 'Dockerfile' -print0 | xargs -0 -n 1 hadolint
 golangci-lint run
 go build ./...
 go test ./...
