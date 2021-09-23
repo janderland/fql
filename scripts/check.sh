c162e8a543e9b7c0c1dc4b6f74a68ab580d2bd6d
Jon Anderson <jon.david.anderson@gmail.com>
2021-09-23 12:15:16 -0700

    Improvement to compose reliability. (#42)

--- scripts/check.sh
+++ scripts/check.sh
@@ -2,8 +2,14 @@
 set -exuo pipefail
 
 cd "${0%/*}/.."
+
+# Lint shell scripts.
 find . -type f -iname '*.sh' -print0 | xargs -0 shellcheck
+
+# Lint Dockerfiles.
 find . -type f -iname 'Dockerfile' -print0 | xargs -0 -n 1 hadolint
+
+# Lint, build, & test Go code.
 golangci-lint run
 go build ./...
 go test ./...
