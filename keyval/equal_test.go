d12886aac81a0575648124c9d6cc4f48a3074aa4
Jon Anderson <jon.david.anderson@gmail.com>
2021-10-10 12:06:44 -0700

    Added tests for Eq methods.

---
+++ keyval/equal_test.go
@@ -0,0 +1,98 @@
+package keyval
+
+import (
+	"math/big"
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func TestNil_Eq(t *testing.T) {
+	x := Nil{}
+	assert.True(t, x.Eq(Nil{}))
+	assert.False(t, x.Eq(nil))
+	assert.False(t, x.Eq(5))
+}
+
+func TestInt_Eq(t *testing.T) {
+	x := Int(5)
+	assert.True(t, x.Eq(Int(5)))
+	assert.False(t, x.Eq(Int(6)))
+	assert.False(t, x.Eq(Float(5)))
+}
+
+func TestUint_Eq(t *testing.T) {
+	x := Uint(12)
+	assert.True(t, x.Eq(Uint(12)))
+	assert.False(t, x.Eq(Uint(2)))
+	assert.False(t, x.Eq(Int(12)))
+}
+
+func TestBool_Eq(t *testing.T) {
+	x := Bool(true)
+	assert.True(t, x.Eq(Bool(true)))
+	assert.False(t, x.Eq(Bool(false)))
+	assert.False(t, x.Eq(Int(0)))
+}
+
+func TestFloat_Eq(t *testing.T) {
+	x := Float(55.2)
+	assert.True(t, x.Eq(Float(55.2)))
+	assert.False(t, x.Eq(Float(22)))
+	assert.False(t, x.Eq(Bool(true)))
+}
+
+func TestBigInt_Eq(t *testing.T) {
+	x := BigInt(*big.NewInt(25))
+	assert.True(t, x.Eq(BigInt(*big.NewInt(25))))
+	assert.False(t, x.Eq(BigInt(*big.NewInt(60009))))
+	assert.False(t, x.Eq(String("hi")))
+}
+
+func TestString_Eq(t *testing.T) {
+	x := String("hi world")
+	assert.True(t, x.Eq(String("hi world")))
+	assert.False(t, x.Eq(String("you")))
+	assert.False(t, x.Eq(Int(-5)))
+}
+
+func TestUUID_Eq(t *testing.T) {
+	x := UUID{0xbc, 0xef, 0xd2, 0xec, 0x4d, 0xf5, 0x43, 0xb6, 0x8c, 0x79, 0x81, 0xb7, 0x0b, 0x88, 0x6a, 0xf9}
+	assert.True(t, x.Eq(UUID{0xbc, 0xef, 0xd2, 0xec, 0x4d, 0xf5, 0x43, 0xb6, 0x8c, 0x79, 0x81, 0xb7, 0x0b, 0x88, 0x6a, 0xf9}))
+	assert.False(t, x.Eq(UUID{0x00, 0x00, 0xd2, 0xec, 0x4d, 0xf5, 0x43, 0xb6, 0x8c, 0x79, 0x81, 0xb7, 0x0b, 0x88, 0x6a, 0xf9}))
+	assert.False(t, x.Eq(String("wow")))
+}
+
+func TestBytes_Eq(t *testing.T) {
+	x := Bytes{0xAB, 0xFF, 0x23}
+	assert.True(t, x.Eq(Bytes{0xAB, 0xFF, 0x23}))
+	assert.False(t, x.Eq(Bytes{0x00, 0xFF, 0x23}))
+	assert.False(t, x.Eq(Uint(20)))
+}
+
+func TestVariable_Eq(t *testing.T) {
+	x := Variable{IntType, StringType, UUIDType}
+	assert.True(t, x.Eq(Variable{IntType, StringType, UUIDType}))
+	assert.False(t, x.Eq(Variable{IntType, BoolType, UUIDType}))
+	assert.False(t, x.Eq(Variable{IntType, StringType}))
+	assert.False(t, x.Eq(Int(0)))
+}
+
+func TestMaybeMore_Eq(t *testing.T) {
+	x := MaybeMore{}
+	assert.True(t, x.Eq(MaybeMore{}))
+	assert.False(t, x.Eq(Int(0)))
+}
+
+func TestTuple_Eq(t *testing.T) {
+	x := Tuple{Int(1), Float(2.2), Tuple{Bool(true), Bool(false)}}
+	assert.True(t, x.Eq(Tuple{Int(1), Float(2.2), Tuple{Bool(true), Bool(false)}}))
+	assert.False(t, x.Eq(Tuple{Int(1), Float(2.2), Tuple{Bool(true), Int(0)}}))
+	assert.False(t, x.Eq(Int(55)))
+}
+
+func TestClear_Eq(t *testing.T) {
+	x := Clear{}
+	assert.True(t, x.Eq(Clear{}))
+	assert.False(t, x.Eq(Bool(false)))
+}
