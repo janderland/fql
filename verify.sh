263000c3217e658b55d80300565fc907083e69cd
Jon Anderson <jon.david.anderson@gmail.com>
2021-11-08 19:56:01 -0800

    Added benchmark script for verify methods. (#53)
    * Added benchmark script for verify methods.
    
    * Got rid of benchmark output file.
    
    * Renamed benchmark script.
    
    * Benchmark logging uses $0 of self name.
    
    * Ignored benchmark out. Clean up verify.sh.

--- verify.sh
+++ verify.sh
@@ -4,5 +4,16 @@ set -exuo pipefail
 # Change directory to repo root.
 cd "${0%/*}"
 
-# Lint, build, & test the codebase.
-docker compose run --rm build "/bin/sh" "-c" "./scripts/setup_database.sh && ./scripts/verify_codebase.sh"
+readonly flag="${1:-}"
+
+if [[ -z "$flag" ]]; then
+  docker compose build verify_built
+  docker compose up verify_built
+  exit $?
+elif [[ "$flag" == "--mounted" ]]; then
+  docker compose up verify_mounted
+  exit $?
+else
+  echo "verify.sh [--mounted]"
+  exit 1
+fi
