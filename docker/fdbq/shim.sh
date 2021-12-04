758c847bbaa162cd07d3b306c75180677e8a02e1
Jon Anderson <jon.david.anderson@gmail.com>
2021-12-03 21:20:32 -0800

    Forgot to include shim.sh.

---
+++ docker/fdbq/shim.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/env bash
+set -euo pipefail
+
+# Expand the contents of the first argument and store
+# for later use as the contents of the cluster file.
+# Shift the rest of the arguments up if there ary any.
+CLUSTER_FILE="$(eval echo "${1:-}")"
+echo "shim.sh - cluster file: $CLUSTER_FILE"
+shift || true
+
+# Create the cluster file and run FDBQ with the
+# remaining arguments.
+echo $CLUSTER_FILE > /etc/foundationdb/fdb.cluster
+/fdbq "$@"
