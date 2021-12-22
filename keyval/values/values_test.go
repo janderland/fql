7847d227f90d8b758227d61aab397cbc95e6dba2
Jon Anderson <jon.david.anderson@gmail.com>
2021-12-21 18:38:35 -0800

    Added tests for non-filter mode.

--- keyval/values/values_test.go
+++ keyval/values/values_test.go
@@ -69,6 +69,39 @@ func TestInvalidUnpack(t *testing.T) {
 	}
 }
 
+func TestNoFilter(t *testing.T) {
+	tests := []struct {
+		name  string
+		query q.Value
+		val   []byte
+		out   q.Value
+		err   bool
+	}{
+		{name: "empty variable", query: q.Variable{}, val: []byte{0xAE, 0xBC}, out: q.Bytes{0xAE, 0xBC}},
+		{name: "variable match", query: q.Variable{q.IntType, q.StringType}, val: []byte("hi"), out: q.String("hi")},
+		{name: "variable mismatch", query: q.Variable{q.IntType}, val: []byte("hi"), err: true},
+		{name: "packed match", query: q.String("you"), val: []byte("you"), out: q.String("you")},
+		{name: "packed mismatch", query: q.Int(22), val: []byte("you"), err: true},
+	}
+
+	for _, test := range tests {
+		t.Run(test.name, func(t *testing.T) {
+			filter, err := NewDeserializer(test.query, binary.BigEndian, false)
+			if !assert.NoError(t, err) {
+				t.FailNow()
+			}
+			val, err := filter(test.val)
+			assert.Equal(t, test.out, val)
+
+			if test.err {
+				assert.Error(t, err)
+			} else {
+				assert.NoError(t, err)
+			}
+		})
+	}
+}
+
 func TestFilter(t *testing.T) {
 	tests := []struct {
 		name  string
