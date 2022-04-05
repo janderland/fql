001da2f0a28428b267974321534dbb0a7021e21d
Jon Anderson <jon.david.anderson@gmail.com>
2022-04-04 22:27:05 -0700

    Implementing formatting for tuple elements.

--- parser/parser/format/format.go
+++ parser/parser/format/format.go
@@ -1,6 +1,7 @@
 package format
 
 import (
+	"encoding/hex"
 	"strings"
 
 	q "github.com/janderland/fdbq/keyval"
@@ -56,4 +57,39 @@ func Variable(v q.Variable) string {
 	b.WriteRune(internal.VarEnd)
 
 	return b.String()
+}
+
+func Hex(in q.Bytes) string {
+	var out strings.Builder
+
+	out.WriteString(internal.HexStart)
+	out.WriteString(hex.EncodeToString(in))
+
+	return out.String()
+}
+
+func String(in q.String) string {
+	var out strings.Builder
+
+	out.WriteRune(internal.StrMark)
+	out.WriteString(string(in))
+	out.WriteRune(internal.StrMark)
+
+	return out.String()
+}
+
+func UUID(in q.UUID) string {
+	var out strings.Builder
+
+	out.WriteString(hex.EncodeToString(in[:4]))
+	out.WriteRune('-')
+	out.WriteString(hex.EncodeToString(in[4:6]))
+	out.WriteRune('-')
+	out.WriteString(hex.EncodeToString(in[6:8]))
+	out.WriteRune('-')
+	out.WriteString(hex.EncodeToString(in[8:10]))
+	out.WriteRune('-')
+	out.WriteString(hex.EncodeToString(in[10:]))
+
+	return out.String()
 }
--- parser/parser/format/formatter.go
+++ parser/parser/format/formatter.go
@@ -1,7 +1,10 @@
 package format
 
 import (
+	"strconv"
+
 	q "github.com/janderland/fdbq/keyval"
+	"github.com/janderland/fdbq/parser/parser/internal"
 )
 
 type query struct {
@@ -10,16 +13,16 @@ type query struct {
 
 var _ q.QueryOperation = &query{}
 
-func (x *query) ForDirectory(dir q.Directory) {
-	x.str = Directory(dir)
+func (x *query) ForDirectory(in q.Directory) {
+	x.str = Directory(in)
 }
 
-func (x *query) ForKey(key q.Key) {
-	x.str = Directory(key.Directory) + Tuple(key.Tuple)
+func (x *query) ForKey(in q.Key) {
+	x.str = Directory(in.Directory) + Tuple(in.Tuple)
 }
 
-func (x *query) ForKeyValue(kv q.KeyValue) {
-	x.str = Directory(kv.Key.Directory) + Tuple(kv.Key.Tuple)
+func (x *query) ForKeyValue(in q.KeyValue) {
+	x.str = Directory(in.Key.Directory) + Tuple(in.Key.Tuple)
 }
 
 type directory struct {
@@ -28,12 +31,12 @@ type directory struct {
 
 var _ q.DirectoryOperation = &directory{}
 
-func (x *directory) ForVariable(v q.Variable) {
-	x.str = Variable(v)
+func (x *directory) ForVariable(in q.Variable) {
+	x.str = Variable(in)
 }
 
-func (x *directory) ForString(str q.String) {
-	x.str = string(str)
+func (x *directory) ForString(in q.String) {
+	x.str = string(in)
 }
 
 type tuple struct {
@@ -42,50 +45,55 @@ type tuple struct {
 
 var _ q.TupleOperation = &tuple{}
 
-func (x *tuple) ForTuple(q.Tuple) {
-
+func (x *tuple) ForTuple(tup q.Tuple) {
+	x.str = Tuple(tup)
 }
 
 func (x *tuple) ForNil(q.Nil) {
-
+	x.str = internal.Nil
 }
 
-func (x *tuple) ForInt(q.Int) {
-
+func (x *tuple) ForInt(in q.Int) {
+	x.str = strconv.FormatInt(int64(in), 10)
 }
 
-func (x *tuple) ForUint(q.Uint) {
-
+func (x *tuple) ForUint(in q.Uint) {
+	x.str = strconv.FormatUint(uint64(in), 10)
 }
 
-func (x *tuple) ForBool(q.Bool) {
-
+func (x *tuple) ForBool(in q.Bool) {
+	if in {
+		x.str = internal.True
+	} else {
+		x.str = internal.False
+	}
 }
 
-func (x *tuple) ForFloat(q.Float) {
-
+func (x *tuple) ForFloat(in q.Float) {
+	x.str = strconv.FormatFloat(float64(in), 'g', 10, 64)
 }
 
 func (x *tuple) ForBigInt(q.BigInt) {
-
+	// TODO: Implement BigInt formatting.
+	panic("not implemented")
 }
 
-func (x *tuple) ForString(q.String) {
-
+func (x *tuple) ForString(in q.String) {
+	x.str = String(in)
 }
 
-func (x *tuple) ForUUID(q.UUID) {
-
+func (x *tuple) ForUUID(in q.UUID) {
+	x.str = UUID(in)
 }
 
-func (x *tuple) ForBytes(q.Bytes) {
-
+func (x *tuple) ForBytes(in q.Bytes) {
+	x.str = Hex(in)
 }
 
-func (x *tuple) ForVariable(q.Variable) {
-
+func (x *tuple) ForVariable(in q.Variable) {
+	x.str = Variable(in)
 }
 
 func (x *tuple) ForMaybeMore(q.MaybeMore) {
-
+	x.str = internal.MaybeMore
 }
