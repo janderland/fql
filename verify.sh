f083499f901d6dcab2e1fbf2faf37c31b138e34c
Jon Anderson <jon.david.anderson@gmail.com>
2022-05-29 14:21:45 -0700

    Slight improvement to docker compose flags.

--- verify.sh
+++ verify.sh
@@ -4,4 +4,4 @@ set -exuo pipefail
 # Change directory to repo root.
 cd "${0%/*}"
 
-docker compose up verify --exit-code-from verify
+docker compose up verify --attach verify --exit-code-from verify
