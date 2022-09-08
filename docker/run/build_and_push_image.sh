1bd9750c233359da6437f5a2e0668b9c32b4c282
Jon Anderson <jon.david.anderson@gmail.com>
2022-09-07 22:43:31 -0700

    Docker improvements (#107)
    * Fixed run dockerfile and scripts.
    
    * Cleaned up shim output.
    
    * Upgraded to Go 1.19.
    
    * Upgraded golangci-lint to fix verify.
    
    * Uncomment pushing of image.

--- docker/run/build_and_push_image.sh
+++ docker/run/build_and_push_image.sh
@@ -3,9 +3,12 @@ set -exuo pipefail
 
 DIR="${0%/*}"
 TAG="janderland/fdbq:$(git rev-parse --short HEAD)"
+LATEST="janderland/fdbq:latest"
 ROOT="$(cd "$DIR"/../..; pwd)"
 WORKDIR="/fdbq"
 
 docker run -v "$ROOT":$WORKDIR -w $WORKDIR janderland/fdbq-build:latest go build
-docker build -t "$TAG" -f "$DIR"/Dockerfile "$ROOT"
-docker push "$TAG"
+docker build -t "$TAG" -t $LATEST -f "$DIR"/Dockerfile "$ROOT"
+
+docker push "$TAG" 
+docker push $LATEST
