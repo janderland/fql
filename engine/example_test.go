4ecf93e22a501b23334935ab3ec90d1379349ea6
Jon Anderson <jon.david.anderson@gmail.com>
2022-11-13 20:23:32 -0800

    Adding example file for engine.

---
+++ engine/example_test.go
@@ -0,0 +1,58 @@
+package engine_test
+
+import (
+	"encoding/binary"
+	"os"
+
+	"github.com/apple/foundationdb/bindings/go/src/fdb"
+	"github.com/apple/foundationdb/bindings/go/src/fdb/directory"
+	"github.com/rs/zerolog"
+
+	"github.com/janderland/fdbq/engine"
+	"github.com/janderland/fdbq/engine/facade"
+	"github.com/janderland/fdbq/keyval"
+)
+
+func Example() {
+	eg := engine.New(
+		facade.NewTransactor(fdb.MustOpenDefault(), directory.Root()),
+		zerolog.New(os.Stdout),
+	)
+
+	key := keyval.Key{
+		Directory: keyval.Directory{keyval.String("hello"), keyval.String("there")},
+		Tuple:     keyval.Tuple{keyval.Float(33.3)},
+	}
+
+	// /hello/there{33.3}=10
+	query := keyval.KeyValue{Key: key, Value: keyval.Int(10)}
+	if err := eg.Set(query, binary.BigEndian); err != nil {
+		panic(err)
+	}
+
+	didWrite, err := eg.Transact(func(e engine.Engine) (interface{}, error) {
+		// /hello/there{33.3}=<>
+		query = keyval.KeyValue{Key: key, Value: keyval.Variable{}}
+		result, err := eg.ReadSingle(query, engine.SingleOpts{ByteOrder: binary.BigEndian})
+		if err != nil {
+			return nil, err
+		}
+		if result != nil {
+			return false, nil
+		}
+
+		// /hello/there{33.3}=15
+		query = keyval.KeyValue{Key: key, Value: keyval.Int(15)}
+		if err := eg.Set(query, binary.BigEndian); err != nil {
+			return nil, err
+		}
+		return true, nil
+	})
+	if err != nil {
+		panic(err)
+	}
+
+	if didWrite != false {
+		panic("didWrite should be false")
+	}
+}
