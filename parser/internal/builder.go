6d609a442e251fd973802f1b403e169d2ad02f5c
Jon Anderson <jon.david.anderson@gmail.com>
2022-12-12 19:08:18 -0800

    Removed keyval alias from builder.

--- parser/internal/builder.go
+++ parser/internal/builder.go
@@ -3,7 +3,7 @@ package internal
 import (
 	"github.com/pkg/errors"
 
-	q "github.com/janderland/fdbq/keyval"
+	"github.com/janderland/fdbq/keyval"
 )
 
 // KeyValBuilder is used by parser.Parser to construct the
@@ -11,22 +11,22 @@ import (
 // keyval.KeyValue directly, so these methods outline all
 // the key-value state changes performed by the parser.
 type KeyValBuilder struct {
-	kv q.KeyValue
+	kv keyval.KeyValue
 }
 
 // Get returns the [keyval.KeyValue] being constructed.
-func (x *KeyValBuilder) Get() q.KeyValue {
+func (x *KeyValBuilder) Get() keyval.KeyValue {
 	return x.kv
 }
 
 // AppendVarToDirectory appends a keyval.Variable to the end of the directory.
 func (x *KeyValBuilder) AppendVarToDirectory() {
-	x.kv.Key.Directory = append(x.kv.Key.Directory, q.Variable{})
+	x.kv.Key.Directory = append(x.kv.Key.Directory, keyval.Variable{})
 }
 
 // AppendPartToDirectory appends a keyval.String to the end of the directory.
 func (x *KeyValBuilder) AppendPartToDirectory(token string) {
-	x.kv.Key.Directory = append(x.kv.Key.Directory, q.String(token))
+	x.kv.Key.Directory = append(x.kv.Key.Directory, keyval.String(token))
 }
 
 // AppendToLastDirPart appends the given string to the last element of the
@@ -34,19 +34,19 @@ func (x *KeyValBuilder) AppendPartToDirectory(token string) {
 // not a keyval.String then this method panics.
 func (x *KeyValBuilder) AppendToLastDirPart(token string) error {
 	i := len(x.kv.Key.Directory) - 1
-	str, ok := x.kv.Key.Directory[i].(q.String)
+	str, ok := x.kv.Key.Directory[i].(keyval.String)
 	if !ok {
 		return errors.Errorf("expected element %d to be string, actually is %T", i, x.kv.Key.Directory[i])
 	}
-	x.kv.Key.Directory[i] = str + q.String(token)
+	x.kv.Key.Directory[i] = str + keyval.String(token)
 	return nil
 }
 
 // AppendToValueVar appends the given keyval.ValueType to the keyval.Variable
 // assigned as the value. If the value is not a keyval.Variable then this
 // method panics.
-func (x *KeyValBuilder) AppendToValueVar(typ q.ValueType) error {
-	val, ok := x.kv.Value.(q.Variable)
+func (x *KeyValBuilder) AppendToValueVar(typ keyval.ValueType) error {
+	val, ok := x.kv.Value.(keyval.Variable)
 	if !ok {
 		return errors.Errorf("expected value to be variable, actually is %T", x.kv.Value)
 	}
@@ -57,21 +57,21 @@ func (x *KeyValBuilder) AppendToValueVar(typ q.ValueType) error {
 // AppendToValueStr appends the given string to the keyval.String assigned
 // as the value. If the value is not a keyval.String then this method panics.
 func (x *KeyValBuilder) AppendToValueStr(token string) error {
-	str, ok := x.kv.Value.(q.String)
+	str, ok := x.kv.Value.(keyval.String)
 	if !ok {
 		return errors.Errorf("expected value to be string, actually is %T", x.kv.Value)
 	}
-	x.kv.Value = str + q.String(token)
+	x.kv.Value = str + keyval.String(token)
 	return nil
 }
 
 // SetKeyTuple sets the tuple portion of the keyval.Key.
-func (x *KeyValBuilder) SetKeyTuple(tup q.Tuple) {
+func (x *KeyValBuilder) SetKeyTuple(tup keyval.Tuple) {
 	x.kv.Key.Tuple = tup
 }
 
 // SetValue sets the value portion of the keyval.KeyValue.
-func (x *KeyValBuilder) SetValue(val q.Value) {
+func (x *KeyValBuilder) SetValue(val keyval.Value) {
 	x.kv.Value = val
 }
 
@@ -80,12 +80,12 @@ func (x *KeyValBuilder) SetValue(val q.Value) {
 // so these methods outline all the key-value state changes
 // performed by the parser.
 type TupBuilder struct {
-	root  q.Tuple
+	root  keyval.Tuple
 	depth int
 }
 
 // Get returns the [keyval.Tuple] being constructed.
-func (x *TupBuilder) Get() q.Tuple {
+func (x *TupBuilder) Get() keyval.Tuple {
 	return x.root
 }
 
@@ -96,8 +96,8 @@ func (x *TupBuilder) Get() q.Tuple {
 // sub-tuple is completed, EndTuple can be called and
 // construction of the previous tuple will be resumed.
 func (x *TupBuilder) StartSubTuple() {
-	_ = x.mutateTuple(func(tup q.Tuple) (q.Tuple, error) {
-		return append(tup, q.Tuple{}), nil
+	_ = x.mutateTuple(func(tup keyval.Tuple) (keyval.Tuple, error) {
+		return append(tup, keyval.Tuple{}), nil
 	})
 	x.depth++
 }
@@ -113,8 +113,8 @@ func (x *TupBuilder) EndTuple() bool {
 }
 
 // Append appends the given element to the keyval.Tuple.
-func (x *TupBuilder) Append(e q.TupElement) {
-	_ = x.mutateTuple(func(tup q.Tuple) (q.Tuple, error) {
+func (x *TupBuilder) Append(e keyval.TupElement) {
+	_ = x.mutateTuple(func(tup keyval.Tuple) (keyval.Tuple, error) {
 		return append(tup, e), nil
 	})
 }
@@ -123,13 +123,13 @@ func (x *TupBuilder) Append(e q.TupElement) {
 // assigned as the last element of the currently constructed tuple.
 // If the last element is not a keyval.String, then this method panics.
 func (x *TupBuilder) AppendToLastElemStr(token string) error {
-	return x.mutateTuple(func(tup q.Tuple) (q.Tuple, error) {
+	return x.mutateTuple(func(tup keyval.Tuple) (keyval.Tuple, error) {
 		i := len(tup) - 1
-		str, ok := tup[i].(q.String)
+		str, ok := tup[i].(keyval.String)
 		if !ok {
 			return nil, errors.Errorf("expected element %d to be string, actually is %T", i, tup[i])
 		}
-		tup[i] = q.String(string(str) + token)
+		tup[i] = keyval.String(string(str) + token)
 		return tup, nil
 	})
 }
@@ -137,10 +137,10 @@ func (x *TupBuilder) AppendToLastElemStr(token string) error {
 // AppendToLastElemVar appends the given keyval.ValueType to the keyval.Variable
 // assigned as the last element of the currently constructed tuple. If the last
 // element is not a keyval.Variable then this method panics.
-func (x *TupBuilder) AppendToLastElemVar(typ q.ValueType) error {
-	return x.mutateTuple(func(tup q.Tuple) (q.Tuple, error) {
+func (x *TupBuilder) AppendToLastElemVar(typ keyval.ValueType) error {
+	return x.mutateTuple(func(tup keyval.Tuple) (keyval.Tuple, error) {
 		i := len(tup) - 1
-		v, ok := tup[i].(q.Variable)
+		v, ok := tup[i].(keyval.Variable)
 		if !ok {
 			return nil, errors.Errorf("expected element %d to be variable, actually is %T", i, tup[i])
 		}
@@ -150,12 +150,12 @@ func (x *TupBuilder) AppendToLastElemVar(typ q.ValueType) error {
 }
 
 // TODO: Don't assign tuple into parent until EndTuple is called.
-func (x *TupBuilder) mutateTuple(f func(q.Tuple) (q.Tuple, error)) error {
-	tuples := []q.Tuple{x.root}
+func (x *TupBuilder) mutateTuple(f func(keyval.Tuple) (keyval.Tuple, error)) error {
+	tuples := []keyval.Tuple{x.root}
 	tup := tuples[0]
 
 	for i := 0; i < x.depth; i++ {
-		tup = tup[len(tup)-1].(q.Tuple)
+		tup = tup[len(tup)-1].(keyval.Tuple)
 		tuples = append(tuples, tup)
 	}
 
