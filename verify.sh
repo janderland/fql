98e41673062f5b5ff38ffe6fcf0bbefe4d5812a3
Jon Anderson <jon.david.anderson@gmail.com>
2023-01-10 20:57:51 -0800

    Fixed error in top level script.

--- verify.sh
+++ verify.sh
@@ -1,13 +1,11 @@
 #!/usr/bin/env bash
-set -euo pipefail
+set -eo pipefail
 
 # Change directory to repo root.
 cd "${0%/*}"
 
-echo "$1"
-
 COMMAND=()
-if [[ "${1:-}" == "--generated" ]]; then
+if [[ "$1" == "--generated" ]]; then
   COMMAND+=('./scripts/verify_generation.sh &&')
 fi
 COMMAND+=('./scripts/setup_database.sh &&')
