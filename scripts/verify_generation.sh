644e69912c5fbd1122fb9f42544903e4c86ffb67
Jon Anderson <jon.david.anderson@gmail.com>
2023-01-10 20:48:17 -0800

    Removed debug logging.

--- scripts/verify_generation.sh
+++ scripts/verify_generation.sh
@@ -5,7 +5,6 @@ set -euo pipefail
 cd "${0%/*}/.."
 
 STATUS="$(git status --short)"
-echo "$STATUS"
 if [[ -n "$STATUS" ]]; then
   echo "ERR! Generated code cannot be verified while there are uncommitted changes."
   exit 1
@@ -15,7 +14,6 @@ fi
 go generate ./...
 
 STATUS="$(git status --short)"
-echo "$STATUS"
 if [[ -n "$STATUS" ]]; then
   echo "ERR! The following generated files are outdated:"
   cut -d' ' -f3 <<< "$STATUS"
