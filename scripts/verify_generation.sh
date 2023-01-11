d29670aaa8ceea24f3dd39c580843877b33d08dd
Jon Anderson <jon.david.anderson@gmail.com>
2023-01-10 20:55:23 -0800

    Improved error message.

--- scripts/verify_generation.sh
+++ scripts/verify_generation.sh
@@ -18,6 +18,11 @@ if [[ -n "$STATUS" ]]; then
   echo "ERR! The following generated files are outdated:"
   cut -d' ' -f3 <<< "$STATUS"
   echo
-  echo "Execute 'go generate ./...'"
+  echo "If this is CI/CD, you'll need to execute"
+  echo "'go generate ./...' on your local machine"
+  echo "then commit and push the changes."
+  echo
+  echo "If this is your local machine then the above"
+  echo "command has already been executed here."
   exit 1
 fi
