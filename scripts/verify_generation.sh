869c70f9359f7e2b617bedeb42fc431500f188fc
Jon Anderson <jon.david.anderson@gmail.com>
2023-01-10 20:46:45 -0800

    Pass verify commands from verify.sh.

--- scripts/verify_generation.sh
+++ scripts/verify_generation.sh
@@ -4,13 +4,21 @@ set -euo pipefail
 # Change directory to repo root.
 cd "${0%/*}/.."
 
+STATUS="$(git status --short)"
+echo "$STATUS"
+if [[ -n "$STATUS" ]]; then
+  echo "ERR! Generated code cannot be verified while there are uncommitted changes."
+  exit 1
+fi
+
 # Run generation and check for differences.
 go generate ./...
 
-STATUS="$(git status --short | cut -d' ' -f3)"
-if [[ -n $STATUS ]]; then
+STATUS="$(git status --short)"
+echo "$STATUS"
+if [[ -n "$STATUS" ]]; then
   echo "ERR! The following generated files are outdated:"
-  echo "$STATUS"
+  cut -d' ' -f3 <<< "$STATUS"
   echo
   echo "Execute 'go generate ./...'"
   exit 1
