c5bcea114cd106742b0e3405ebc07657b61d3a32
Jon Anderson <jon.david.anderson@gmail.com>
2023-01-13 19:22:09 -0800

    131 define fdb version in env file (#132)
    * Verify build added to compose file.
    
    * Build the build container in Docker Compose.

--- verify.sh
+++ verify.sh
@@ -4,8 +4,28 @@ set -eo pipefail
 # Change directory to repo root.
 cd "${0%/*}"
 
+while [[ $# -gt 0 ]]; do
+  case $1 in
+    --generated)
+      GENERATED="x"
+      shift
+      ;;
+
+    --build)
+      BUILD="x"
+      shift
+      ;;
+  esac
+done
+
+if [[ -n "$BUILD" ]]; then
+  set -x
+  COMMAND="" docker compose build
+  set +x
+fi
+
 COMMAND=()
-if [[ "$1" == "--generated" ]]; then
+if [[ -n "$GENERATED" ]]; then
   COMMAND+=('./scripts/verify_generation.sh &&')
 fi
 COMMAND+=('./scripts/setup_database.sh &&')
