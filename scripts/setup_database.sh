2e78f8f9c5c1485b0c072497c06b410a88230053
Jon Anderson <jon.david.anderson@gmail.com>
2023-02-12 08:56:32 -0800

    Build readme. (#134)
    * Removed build flag from verify.sh.
    
    * Added fdbq service to compose file.
    
    * Added support for other docker commands.
    
    * Renamed verify.sh to docker.sh.
    
    * Added help message.
    
    * docker.sh => build.sh
    
    * Fixed comment.
    
    * Improvements to comments.
    
    * Move fdbq Docker build into compose.
    
    * Added default build tag to build script.
    
    * Define docker tags from git hash.
    
    * Fixed fdbq Docker build.
    
    * Got build tags working.
    
    * Fixed linting err. Improved logging of build.sh.
    
    * Fix linting errors.
    
    * Minor changes to database script.
    
    * Have setup_dabatase.sh fail if FDB is down.
    
    * Removed old unused script.
    
    * Fix typos in build.sh help.
    
    * Changed build.sh flag name.

--- scripts/setup_database.sh
+++ scripts/setup_database.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
-set -u
+set -ue
 
 # The first argument is the hostname of the FDB container.
 FDB_HOSTNAME=${1:-fdb}
@@ -23,9 +23,10 @@ echo "${FDB_DESCRIPTION_ID}@${FDB_IP}:4500" > $FDB_CLUSTER_FILE
 echo "FDB_CLUSTER_FILE: $(cat $FDB_CLUSTER_FILE)"
 
 # Search for the "unreadable_configuration" message in the cluster's status. This message
-# would let us know that the database hasn't been initialized.
-jq -e '.cluster.messages[] | select(.name | contains("unreadable_configuration"))' <(fdbcli --exec 'status json') >&2
-JQ_CODE=$?
+# would let us know that the database hasn't been initialized. We need to disable the '-e'
+# flag so that bash won't exit if jq returns a non-zero code.
+JQ_CODE=0
+jq -e '.cluster.messages[] | select(.name | contains("unreadable_configuration"))' <(set -x; fdbcli --exec 'status json') || JQ_CODE=$?
 
 # jq should only return codes between 0 & 4 inclusive. Our particular query never
 # returns 'null' or 'false', so we shouldn't see code 1. Codes 2 & 3 occur on
@@ -33,18 +34,12 @@ JQ_CODE=$?
 # code is not 0 or 4 then something unexpected happened so return early.
 # https://stedolan.github.io/jq/manual/#Invokingjq
 if [[ $JQ_CODE -lt 0 || ( $JQ_CODE -gt 0 && $JQ_CODE -lt 4 ) || $JQ_CODE -gt 4 ]]; then
-  echo "ERR! jq exited with code $JQ_CODE."
+  echo "ERR! Unexpected jq exit code $JQ_CODE"
   exit "$JQ_CODE"
 fi
 
 # If this is a new instance of FDB, configure the database.
 # https://apple.github.io/foundationdb/administration.html#re-creating-a-database
 if [[ $JQ_CODE -eq 0 ]]; then
-  set -x
-  fdbcli --exec "configure new single memory"
-  set +x
+  (set -x; fdbcli --exec "configure new single memory")
 fi
-
-# If we make it this far then jq should have returned code 4
-# which means there's no need to configure the database and
-# the script can exit.
