aca2e9d6d4151594108992a063a2fa7079d68129
Jon Anderson <jon.david.anderson@gmail.com>
2023-03-19 15:24:23 -0700

    Removed unnecessary indirection.

--- internal/app/app.go
+++ internal/app/app.go
@@ -26,34 +26,30 @@ var Fdbq = &cobra.Command{
 	Use:   "fdbq [flags] query ...",
 	Short: "fdbq is a query language for Foundation DB",
 	RunE: func(_ *cobra.Command, args []string) error {
-		return run(flags, args, os.Stdout, os.Stderr)
+		log := zerolog.Nop()
+		if flags.Log {
+			writer := zerolog.ConsoleWriter{Out: os.Stderr}
+			writer.FormatLevel = func(_ interface{}) string { return "" }
+			log = zerolog.New(writer).With().Timestamp().Logger()
+		}
+
+		log.Log().Str("cluster file", flags.Cluster).Msg("connecting to DB")
+		if err := fdb.APIVersion(620); err != nil {
+			return errors.Wrap(err, "failed to set FDB API version")
+		}
+		db, err := fdb.OpenDatabase(flags.Cluster)
+		if err != nil {
+			return errors.Wrap(err, "failed to connect to DB")
+		}
+
+		app := headless.App{
+			Format: format.New(format.Cfg{
+				PrintBytes: flags.Bytes,
+			}),
+			Flags: *flags,
+			Log:   log,
+			Out:   os.Stdout,
+		}
+		return app.Run(context.Background(), facade.NewTransactor(db, directory.Root()), args)
 	},
-}
-
-func run(flags *flag.Flags, queries []string, stdout *os.File, stderr *os.File) error {
-	log := zerolog.Nop()
-	if flags.Log {
-		writer := zerolog.ConsoleWriter{Out: stderr}
-		writer.FormatLevel = func(_ interface{}) string { return "" }
-		log = zerolog.New(writer).With().Timestamp().Logger()
-	}
-
-	log.Log().Str("cluster file", flags.Cluster).Msg("connecting to DB")
-	if err := fdb.APIVersion(620); err != nil {
-		return errors.Wrap(err, "failed to set FDB API version")
-	}
-	db, err := fdb.OpenDatabase(flags.Cluster)
-	if err != nil {
-		return errors.Wrap(err, "failed to connect to DB")
-	}
-
-	app := headless.App{
-		Format: format.New(format.Cfg{
-			PrintBytes: flags.Bytes,
-		}),
-		Flags: *flags,
-		Log:   log,
-		Out:   stdout,
-	}
-	return app.Run(context.Background(), facade.NewTransactor(db, directory.Root()), queries)
 }
