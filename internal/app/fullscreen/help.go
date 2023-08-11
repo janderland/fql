edf9082a54b842361d0f70c91e343153031868ff
Jon Anderson <jon.david.anderson@gmail.com>
2023-08-11 12:29:44 -0400

    Added keymapping to help message.

--- internal/app/fullscreen/help.go
+++ internal/app/fullscreen/help.go
@@ -11,26 +11,32 @@ var helpMsg string
 
 func init() {
 	const str = `
-FDBQ provides an interactive environment for interacting
-with Foundation DB. The environment has 3 modes: input,
-scroll, & help. The environment starts in input mode.
-Ctrl+C always quits the program, regardless of the
-current mode.
+FDBQ provides an environment for reading & writing
+key-values in a Foundation DB cluster. The environment
+has 3 modes: input, scroll, & help. The environment
+starts in input mode. Ctrl+C always quits the program,
+regardless of the current mode.
 
 During input mode, the user can type queries into the
 input box at the bottom of the screen. Pressing "enter"
 cancels the currently executing query, clears the on
 screen results, and executes a new query defined by
-the input box. Pressing "escape" switches to scroll
-mode.
+the input box. Pressing "up" or "down" scrolls by line.
+Pressing "page up" or "page down" scrolls by page.
+Pressing "escape" switches to scroll mode.
 
 During scroll mode, the user can scroll through the
-results of the previously executed query. Pressing "i"
+results of the previously executed query. Pressing
+"up", "down", "page up", or "page down" scrolls as in
+input mode. Pressing "j" or "k" scrolls by line.
+Pressing "J" or "K" scrolls by item. Pressing "ctrl+d"
+or "ctrl+u" scrolls by half page. Pressing "i"
 switches back to input mode. Pressing "?" switches to
 help mode.
 
 During help mode, this help screen is displayed.
-Pressing "escape" switches to scroll mode.
+Scrolling works the same as in scroll mode. Pressing
+"escape" switches to scroll mode.
 `
 
 	// Remove leading & trailing whitespace.
